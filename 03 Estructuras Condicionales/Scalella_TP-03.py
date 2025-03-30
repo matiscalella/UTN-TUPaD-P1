efb6a9635b6588da6a5b451c655387664b70d8d7
Matias Scalella <matias.scalella@gmail.com>
2025-03-30 18:04:55 -0300

    Inicio TP3 hasta el ejercicio 5

---
+++ 03 Estructuras Condicionales/Scalella_TP-03.py
@@ -0,0 +1,47 @@
+# TP3 Matias Scalella
+# Ejercicio 1
+"""
+edad = int(input("Hola, ingrese su edad: "))
+if edad >= 18:
+    print("Es mayor de edad")
+elif edad < 18 and edad >=0:
+    print("No es mayor de edad")
+else:
+    print("Reinicie el programa.")
+
+# Ejercicio 2
+nota = float(input("Ingrese su nota: "))
+mensaje = "Aprobado" if nota >= 6 else "Desaprobado"
+print(mensaje)
+
+# Ejercicio 3
+numeroIngresado = float(input("Ingrese un numero par: "))
+if numeroIngresado % 2 == 0:
+    print("Ha ingresado un número par")
+else:
+    print("Por favor, ingrese un número par")
+
+# Ejercicio 4
+edad = int(input("Ingrese su edad: "))
+if edad <= 0:
+    print("Ingrese su edad correctamente, reinicie el programa")
+elif edad > 0 and edad < 12:
+    print ("Niño/a")
+elif edad >= 12 and edad < 18:
+    print("Adolescente")
+elif edad >= 18 and edad < 30:
+    print("Adulto/a joven")
+elif edad >= 30:
+    print("Adulto/a")
+"""
+# Ejercicio 5
+
+# Ejercicio 6
+
+# Ejercicio 7
+
+# Ejercicio 8
+
+# Ejercicio 9
+
+# Ejercicio 10
