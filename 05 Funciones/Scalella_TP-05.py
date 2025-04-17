86a1071cdbb35560504a8bddd2382994e01f4bcc
Matias Scalella <matias.scalella@gmail.com>
2025-04-16 22:49:33 -0300

    Agrego ejercicios 1-4 de Funciones

---
+++ 05 Funciones/Scalella_TP-05.py
@@ -0,0 +1,64 @@
+"""# Ejercicio 1
+# Funciones
+def imprimir_hola_mundo():
+    print("Hola Mundo!")
+
+# Programa Principal
+imprimir_hola_mundo()
+
+# Ejercicio 2
+# Funciones
+def saludar_usuario(nombre):
+    print(f"Hola {nombre}!")
+
+# Programa Principal
+nombre = input("Bienvenido, ingresa tu nombre: ")
+saludar_usuario(nombre)
+
+# Ejercicio 3
+# Funciones
+def informacion_personal(nombre, apellido, edad, residencia):
+    print(f"Hola soy {nombre} {apellido}, tengo {edad} años y vivo en {residencia}")
+
+# Programa Principal
+nombre = input("Ingresa tu nombre: ")
+apellido = input("Ingresa tu apellido: ")
+edad = input("Ingresa tu edad: ")
+residencia = input("Ingresa tu lugar de residencia: ")
+informacion_personal(nombre, apellido, edad, residencia)
+
+# Ejercicio 4
+# Funciones
+import math
+def calcular_area_circulo(radio):
+    return math.pi * (radio ** 2)
+
+def calcular_perimetro_circulo(radio):
+    return 2 * math.pi * radio
+
+# Programa Principal
+radio = float(input("Hola, ingresa el radio del circulo: "))
+area = round(calcular_area_circulo(radio), 2)
+perimetro = round(calcular_perimetro_circulo(radio), 2)
+print(f"El area del circulo es {area}")
+print(f"El perimetro del circulo es {perimetro}")
+"""
+# Ejercicio 5
+# Funciones
+
+# Programa Principal
+
+# Ejercicio 
+# Funciones
+
+# Programa Principal
+
+# Ejercicio 
+# Funciones
+
+# Programa Principal
+
+# Ejercicio 
+# Funciones
+
+# Programa Principal
